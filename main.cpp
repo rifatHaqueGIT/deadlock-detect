af2062ab867e72f13071bf226c1fb1efba0a1876
pfederl <pfederl@ucalgary.ca>
2020-06-08 16:50:31 -0600

    ...

--- main.cpp
+++ main.cpp
@@ -2,25 +2,7 @@
 #include <tuple>
 #include <string>
 
-class Deadlock {
- public:
-  std::tuple<bool,std::string>
-  lock( std::string process, std::string resource);
-};
-
 int main()
 {
-  Deadlock dlock;
-  bool res;
-  std::string msg;
-  std::tie(res, msg) = dlock.lock("dude", "7");
-
-  std::cout << res << "," << msg << "\n";
 }
 
-
-std::tuple<bool,std::string>
-Deadlock::lock( std::string process, std::string resource)
-{
-  return { false, "Bad boy" };
-}
